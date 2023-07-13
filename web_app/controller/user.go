c9de293924753f581ba86185120166331ed29530
Maker-Wu <1325732547@qq.com>
2023-07-13 10:02:46 +0800

    refactor viper

---
+++ web_app/controller/user.go
@@ -0,0 +1,9 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func SignUpController(ctx *gin.Context) {
+	// 1. 参数校验
+	// 2. 业务处理
+	// 3. 返回响应
+}
--- web_app/router/routes.go
+++ web_app/router/routes.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"bookstore/web_app/controller"
 	"bookstore/web_app/logger"
 	"net/http"
 
@@ -11,8 +12,17 @@ func Setup() *gin.Engine {
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 
-	r.Handle(http.MethodGet, "/", func(ctx *gin.Context) {
+	// 注册业务路由
+	r.POST("/signup", controller.SignUpController)
 
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "pong")
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "404",
+		})
 	})
 
 	return r
